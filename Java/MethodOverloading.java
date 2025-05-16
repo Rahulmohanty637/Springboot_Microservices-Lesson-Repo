8ff3973a16dec8c4e1a9fac2af6f603d0b7d6b5c
Rahul Mohanty <130064642+Rahulmohanty637@users.noreply.github.com>
2025-05-16 14:58:41 +0530

    Create MethodOverloading.java

---
+++ Java/MethodOverloading.java
@@ -0,0 +1,21 @@
+public class MethodOverloading {
+
+    public static void main(String[] args) {
+
+        Calculator cal = new Calculator();
+        System.out.println(cal.add(4, 5));
+    }
+
+}
+
+    class Calculator {
+
+        public int add(int a, int b) {
+            return (a + b);
+        }
+
+        public int add(int a, int b, int c) {
+            return (a + b + c);
+        }
+
+    }
