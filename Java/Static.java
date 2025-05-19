67df591eb0cfc77f5cd4e60e8ed31b6c89e8b624
Rahul Mohanty <130064642+Rahulmohanty637@users.noreply.github.com>
2025-05-19 15:25:20 +0530

    Update Static.java

--- Java/Static.java
+++ Java/Static.java
@@ -23,7 +23,7 @@ class Mobile {
 
     String brand;
     int price;
-    static String name;
+    static String name; 
 
     public void show() {
         System.out.println(brand + " : " + price + " : " + name);
