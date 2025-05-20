002310b008d95fa3389f968e0617c2d4069a3f55
Rahul Mohanty <130064642+Rahulmohanty637@users.noreply.github.com>
2025-05-20 19:00:25 +0530

    Hierarchy

--- Java/Calculator.java
+++ Java/Calculator.java
@@ -1,10 +1,11 @@
 public class Calculator {
     public static void main(String[] args) {
-        AdvCalc obj = new AdvCalc();
+        VeryAdvCalc obj = new VeryAdvCalc();
         int r1 = obj.add(49, 0);
         int r2 = obj.sub(10, 7);
         int r3 = obj.multi(12, 7);
         int r4 = obj.divide(14, 2);
-        System.out.println(r1 + " " + r2 + " " + r3 + " " + r4);
+        double r5 = obj.powFunction(4, 2);
+        System.out.println(r1 + " " + r2 + " " + r3 + " " + r4 + " " + r5);
     }
 }
---
+++ Java/VeryAdvCalc.java
@@ -0,0 +1,5 @@
+public class VeryAdvCalc extends AdvCalc {
+    public double powFunction(int x, int y) {
+        return Math.pow(x, y);
+    }
+}
