1f532d4c5eb3af395883996bc7162c00dbdacf0f
Rahul Mohanty <130064642+Rahulmohanty637@users.noreply.github.com>
2025-05-21 15:54:25 +0530

    This and Super Method

---
+++ Java/ThisAndSuperMethod.java
@@ -0,0 +1,30 @@
+public class ThisAndSuperMethod {
+    public static void main(String[] args) {
+        B obj = new B(6);
+
+    }
+}
+
+class A {
+    public A() {
+        System.out.println("in A");
+    }
+
+    public A(int n) {
+        this();
+        System.out.println("in A int");
+    }
+}
+
+class B extends A {
+
+    public B() {
+        super(5);
+        System.out.println("in B");
+    }
+
+    public B(int n) {
+        this();
+        System.out.println("in B int");
+    }
+}
