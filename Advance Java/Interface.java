1dfa79f8b473adec9d46cf630fb840a25ebb29e5
Rahul Mohanty <130064642+Rahulmohanty637@users.noreply.github.com>
2025-05-23 18:26:31 +0530

    Interfarce

--- Advance Java/Interface.java
+++ Advance Java/Interface.java
@@ -1,5 +1,34 @@
 // Interface
 
+interface A {
+    // instance variables are final and static
+    int age = 24;
+    String location = "Bangalore";
+
+    void show();
+
+    void config();
+}
+
+class B implements A {
+
+    @Override
+    public void show() {
+        System.out.println("In show");
+    }
+
+    @Override
+    public void config() {
+        System.out.println("In config");
+    }
+
+}
+
 public class Interface {
-    
+    public static void main(String[] args) {
+        B obj = new B();
+        obj.config();
+        obj.show();
+        System.out.println(A.location);
+    }
 }
