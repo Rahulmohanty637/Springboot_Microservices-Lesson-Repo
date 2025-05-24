0ff9f08be43aa3b60c5678afc5e7f1256f8072a5
Rahul Mohanty <130064642+Rahulmohanty637@users.noreply.github.com>
2025-05-24 23:34:12 +0530

    Enum if else and switch

---
+++ Advance Java/EnumIfAndSwitch.java
@@ -0,0 +1,36 @@
+enum Status {
+    Running, Pending, Failed, Success;
+}
+
+public class EnumIfAndSwitch {
+    public static void main(String[] args) {
+
+        Status s = Status.Failed;
+
+        // If Else statement
+        if (s == Status.Success) {
+            System.out.println("Success");
+        } else if (s == Status.Pending || s == Status.Running) {
+            System.out.println("Pending or Running");
+        } else {
+            System.out.println("Try Again");
+        }
+
+        // Switch
+        switch (s) {
+            case Running:
+                System.out.println("All Good");
+                break;
+            case Failed:
+                System.out.println("Try Again");
+                break;
+            case Pending:
+                System.out.println("Please Wait");
+                break;
+            default:
+                System.out.println("Success");
+                break;
+        }
+
+    }
+}
