2660d46597831971ae1632d62cb96eae2851a1e6
Rahul Mohanty <130064642+Rahulmohanty637@users.noreply.github.com>
2025-05-29 02:13:32 +0530

    Throw and Throws Keywords

---
+++ Advance Java/ThrowAndThrows.java
@@ -0,0 +1,21 @@
+public class ThrowAndThrows {
+    public static void main(String[] args) {
+        int i = 20;
+        int j = 0;
+
+        try {
+            j = 18 / i;
+            if (j == 0) {
+                throw new ArithmeticException("Don't want to print zero");
+            }
+        } catch (ArithmeticException e) {
+            j = 18 / 1;
+            System.out.println("Thats the default output " + e);
+        } catch (Exception e) {
+            System.out.println("Something went wrong...");
+        }
+
+        System.out.println(j);
+        System.out.println("Bye");
+    }
+}
