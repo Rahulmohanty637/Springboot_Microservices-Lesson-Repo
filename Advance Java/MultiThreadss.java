3f891f2f08ee0444933f740dd50d8c6f76f0b091
Rahul Mohanty <130064642+Rahulmohanty637@users.noreply.github.com>
2025-06-01 22:13:22 +0530

    Runnable Interface

--- Advance Java/MultiThreadss.java
+++ Advance Java/MultiThreadss.java
@@ -2,6 +2,11 @@ class A extends Thread {
     public void run() {
         for (int i = 0; i < 100; i++) {
             System.out.println("HI");
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
@@ -10,6 +15,24 @@ class B extends Thread {
     public void run() {
         for (int i = 0; i < 100; i++) {
             System.out.println("Hello");
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+class C implements Runnable {
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Hello");
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
@@ -18,9 +41,31 @@ public class MultiThreadss {
     public static void main(String[] args) {
         A obj1 = new A();
         B obj2 = new B();
+        Runnable obj3 = new C();
 
-        System.out.println(obj1.getPriority());
-        obj1.start();
-        obj2.start();
+        // obj2.setPriority(Thread.MAX_PRIORITY);
+        // System.out.println(obj1.getPriority());
+        // obj1.start();
+        // obj2.start();
+
+        // Thread t1 = new Thread(obj3);
+        // t1.start();
+
+        Runnable obj4 = new Runnable() {
+            public void run() {
+                for (int i = 0; i < 5; i++) {
+                    System.out.println("Hello");
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+
+         Thread t2 = new Thread(obj4);
+        t2.start();
+
     }
 }
