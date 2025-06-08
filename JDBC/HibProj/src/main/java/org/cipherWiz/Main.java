13d14f594f79a2cba1c85a3574c68162144cffd8
Rahul Mohanty <130064642+Rahulmohanty637@users.noreply.github.com>
2025-06-08 12:48:34 +0530

    Merging 2 tables with Embeddable annotation

--- JDBC/HibProj/src/main/java/org/cipherWiz/Main.java
+++ JDBC/HibProj/src/main/java/org/cipherWiz/Main.java
@@ -9,9 +9,41 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        Laptop l1 = new Laptop();
+        l1.setRam(16);
+        l1.setBrand("Asus");
+        l1.setModel("Rog");
+
+        Alien a1 = new Alien();
+        a1.setAid(111);
+        a1.setAname("Rahul");
+        a1.setTech("Java");
+        a1.setLaptop(l1);
+
+
 //        Configuration cfg = new Configuration();
 //        cfg.addAnnotatedClass(org.cipherWiz.Student.class);
 //        cfg.configure();
+
+        SessionFactory sf = new Configuration()
+                .addAnnotatedClass(org.cipherWiz.Alien.class)
+                .configure()
+                .buildSessionFactory();      // cfg.buildSessionFactory();
+
+        Session session = sf.openSession();
+        Transaction transaction = session.beginTransaction();
+
+        session.persist(a1);
+        transaction.commit();
+
+        Alien a2 = session.get(Alien.class, 111);
+        System.out.println(a2);
+        session.close();
+
+    }
+
+    public static void crud1() {
 
         SessionFactory sf = new Configuration()
                 .addAnnotatedClass(org.cipherWiz.Student.class)
