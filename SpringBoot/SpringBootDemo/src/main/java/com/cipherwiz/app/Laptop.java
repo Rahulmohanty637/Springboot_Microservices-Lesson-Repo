38f0a2a5c12a4c34f0c681355afe903c48ae8a5b
Rahul Mohanty <130064642+Rahulmohanty637@users.noreply.github.com>
2025-06-11 16:00:37 +0530

    Wiring

---
+++ SpringBoot/SpringBootDemo/src/main/java/com/cipherwiz/app/Laptop.java
@@ -0,0 +1,11 @@
+package com.cipherwiz.app;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptop {
+
+    public void compiling(){
+        System.out.println("Compiling...");
+    }
+}
--- SpringBoot/SpringBootDemo/src/main/java/com/cipherwiz/app/SpringBootDemoApplication.java
+++ SpringBoot/SpringBootDemo/src/main/java/com/cipherwiz/app/SpringBootDemoApplication.java
@@ -2,13 +2,17 @@ package com.cipherwiz.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringBootDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootDemoApplication.class, args);
-		System.out.println("Hello World");
+		ApplicationContext context = SpringApplication.run(SpringBootDemoApplication.class, args);
+
+		Alien obj = context.getBean(Alien.class);
+		obj.code();
+
 	}
 
 }
