6cffe74a7ed79d133da6ba8e8d4bb6c14b36e702
Rahul Mohanty <130064642+Rahulmohanty637@users.noreply.github.com>
2025-06-13 23:28:37 +0530

    Update Desktop.java

--- Spring Framework/Spring2/src/main/java/org/example/Desktop.java
+++ Spring Framework/Spring2/src/main/java/org/example/Desktop.java
@@ -1,5 +1,8 @@
 package org.example;
 
-public class Desktop {
-
+public class Desktop implements Computer {
+    @Override
+    public void compile(){
+        System.out.println("Compiling using Desktop!");
+    }
 }
