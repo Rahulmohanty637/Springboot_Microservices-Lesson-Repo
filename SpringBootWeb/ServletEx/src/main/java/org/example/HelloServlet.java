a603e98550fe043d12ca198d92b416f67fd5daea
Rahul Mohanty <130064642+Rahulmohanty637@users.noreply.github.com>
2025-06-17 23:15:21 +0530

    Servlet Mapping

--- SpringBootWeb/ServletEx/src/main/java/org/example/HelloServlet.java
+++ SpringBootWeb/ServletEx/src/main/java/org/example/HelloServlet.java
@@ -1,12 +1,14 @@
 package org.example;
 
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 public class HelloServlet extends HttpServlet {
 
     public void service(HttpServletRequest req, HttpServletResponse res){
         System.out.println("In Service");
+
     }
 }
