609d51c3177a716f84405953e71574c51dc4199c
Rahul Mohanty <130064642+Rahulmohanty637@users.noreply.github.com>
2025-06-17 23:57:17 +0530

    port changed

--- SpringBootWeb/ServletEx/src/main/java/org/example/HelloServlet.java
+++ SpringBootWeb/ServletEx/src/main/java/org/example/HelloServlet.java
@@ -5,10 +5,12 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.IOException;
+
 public class HelloServlet extends HttpServlet {
 
-    public void service(HttpServletRequest req, HttpServletResponse res){
+    public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
         System.out.println("In Service");
-
+        res.getWriter().println("Hello");
     }
 }
