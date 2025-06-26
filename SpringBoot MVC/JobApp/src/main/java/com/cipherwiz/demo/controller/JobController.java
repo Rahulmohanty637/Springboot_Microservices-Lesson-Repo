e42f2bef6b34a97624cfc4d573ee83d932a39e36
Rahul Mohanty <130064642+Rahulmohanty637@users.noreply.github.com>
2025-06-26 22:58:06 +0530

    Update JobController.java

--- SpringBoot MVC/JobApp/src/main/java/com/cipherwiz/demo/controller/JobController.java
+++ SpringBoot MVC/JobApp/src/main/java/com/cipherwiz/demo/controller/JobController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -39,4 +40,6 @@ public class JobController {
         m.addAttribute("jobPosts", jobs);
         return "viewalljobs";
     }
+
+
 }
