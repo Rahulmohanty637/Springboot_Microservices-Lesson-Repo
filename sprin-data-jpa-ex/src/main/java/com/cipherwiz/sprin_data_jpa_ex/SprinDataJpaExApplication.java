fd7625b4fa97999f0a1f114452df6a6289d77158
Rahul Mohanty <130064642+Rahulmohanty637@users.noreply.github.com>
2025-07-17 01:08:01 +0530

    JPA

---
+++ sprin-data-jpa-ex/src/main/java/com/cipherwiz/sprin_data_jpa_ex/SprinDataJpaExApplication.java
@@ -0,0 +1,13 @@
+package com.cipherwiz.sprin_data_jpa_ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinDataJpaExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinDataJpaExApplication.class, args);
+	}
+
+}
---
+++ sprin-data-jpa-ex/src/test/java/com/cipherwiz/sprin_data_jpa_ex/SprinDataJpaExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cipherwiz.sprin_data_jpa_ex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinDataJpaExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
