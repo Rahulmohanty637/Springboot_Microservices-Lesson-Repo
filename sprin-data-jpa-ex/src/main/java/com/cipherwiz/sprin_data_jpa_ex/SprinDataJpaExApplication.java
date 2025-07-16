4540570ca27a8c673fd22ab34147e59be06e6356
Rahul Mohanty <130064642+Rahulmohanty637@users.noreply.github.com>
2025-07-17 01:27:52 +0530

    repo

--- sprin-data-jpa-ex/src/main/java/com/cipherwiz/sprin_data_jpa_ex/SprinDataJpaExApplication.java
+++ sprin-data-jpa-ex/src/main/java/com/cipherwiz/sprin_data_jpa_ex/SprinDataJpaExApplication.java
@@ -1,13 +1,34 @@
 package com.cipherwiz.sprin_data_jpa_ex;
 
+import com.cipherwiz.sprin_data_jpa_ex.model.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SprinDataJpaExApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SprinDataJpaExApplication.class, args);
+		ApplicationContext context = SpringApplication.run(SprinDataJpaExApplication.class, args);
+
+		StudentRepo repo = context.getBean(StudentRepo.class);
+		
+		Student s1 = context.getBean(Student.class);
+		Student s2 = context.getBean(Student.class);
+		Student s3 = context.getBean(Student.class);
+
+		s1.setRollno(101);
+		s1.setName("Navin");
+		s1.setMarks(75);
+
+		s2.setRollno(102);
+		s2.setName("Viren");
+		s2.setMarks(52);
+
+		s3.setRollno(103);
+		s3.setName("Kapil");
+		s3.setMarks(67);
+
 	}
 
 }
---
+++ sprin-data-jpa-ex/src/main/java/com/cipherwiz/sprin_data_jpa_ex/StudentRepo.java
@@ -0,0 +1,9 @@
+package com.cipherwiz.sprin_data_jpa_ex;
+
+import com.cipherwiz.sprin_data_jpa_ex.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepo extends JpaRepository<Student, Integer> {
+}
