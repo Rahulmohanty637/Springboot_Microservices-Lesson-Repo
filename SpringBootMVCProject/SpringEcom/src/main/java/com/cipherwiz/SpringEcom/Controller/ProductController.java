b4dde0492792e50832c7c682522f788d38117424
Rahul Mohanty <130064642+Rahulmohanty637@users.noreply.github.com>
2025-07-21 01:31:03 +0530

    Search feature added

--- SpringBootMVCProject/SpringEcom/src/main/java/com/cipherwiz/SpringEcom/Controller/ProductController.java
+++ SpringBootMVCProject/SpringEcom/src/main/java/com/cipherwiz/SpringEcom/Controller/ProductController.java
@@ -76,4 +76,11 @@ public class ProductController {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
+
+    @GetMapping("/products/search")
+    public ResponseEntity<List<Product>> searchProducts(@RequestParam String keyword){
+        List<Product> products = productService.searchProducts(keyword);
+        System.out.println("searching with " + keyword);
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
 }
