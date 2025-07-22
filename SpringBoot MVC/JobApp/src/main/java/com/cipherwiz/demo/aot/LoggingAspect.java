34b530d7c21ea257e0ebedcf4d3811af32045129
Rahul Mohanty <130064642+Rahulmohanty637@users.noreply.github.com>
2025-07-23 00:08:42 +0530

    AOT Introduction

---
+++ SpringBoot MVC/JobApp/src/main/java/com/cipherwiz/demo/aot/LoggingAspect.java
@@ -0,0 +1,9 @@
+package com.cipherwiz.demo.aot;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LoggingAspect {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(LoggingAspect.class);
+}
