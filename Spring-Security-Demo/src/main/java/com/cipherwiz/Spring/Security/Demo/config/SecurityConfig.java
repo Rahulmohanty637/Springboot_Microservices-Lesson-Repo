c0df083db2f07fac2fa507b63e77aee1283d8c27
Rahul Mohanty <130064642+Rahulmohanty637@users.noreply.github.com>
2025-08-04 23:52:11 +0530

    Stateless with lambda function

--- Spring-Security-Demo/src/main/java/com/cipherwiz/Spring/Security/Demo/config/SecurityConfig.java
+++ Spring-Security-Demo/src/main/java/com/cipherwiz/Spring/Security/Demo/config/SecurityConfig.java
@@ -2,7 +2,10 @@ package com.cipherwiz.Spring.Security.Demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,7 +16,17 @@ public class SecurityConfig {
     // means we have to create a bean which will return SecurityFilterChain object
     // So entire Spring security behind the scenes works with this spring SecurityFilterChain using HttpSecurity
     @Bean
-    public SecurityFilterChain securityFilterChain(){
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
+        
+
+
+//        http.csrf(customizer -> customizer.disable());
+//        http.authorizeHttpRequests(request -> request.anyRequest().authenticated());
+////        http.formLogin(Customizer.withDefaults());
+//        http.httpBasic(Customizer.withDefaults());
+//        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
     }
 }
