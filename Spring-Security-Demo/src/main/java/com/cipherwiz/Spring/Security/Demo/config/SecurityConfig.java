c08681ccec021a032c99ba20402b95489645040f
Rahul Mohanty <130064642+Rahulmohanty637@users.noreply.github.com>
2025-08-04 23:19:49 +0530

    Update SecurityConfig.java

--- Spring-Security-Demo/src/main/java/com/cipherwiz/Spring/Security/Demo/config/SecurityConfig.java
+++ Spring-Security-Demo/src/main/java/com/cipherwiz/Spring/Security/Demo/config/SecurityConfig.java
@@ -1,4 +1,19 @@
 package com.cipherwiz.Spring.Security.Demo.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    // When you want to change something you need to return an object of SercurityFilterchain, that
+    // means we have to create a bean which will return SecurityFilterChain object
+    // So entire Spring security behind the scenes works with this spring SecurityFilterChain using HttpSecurity
+    @Bean
+    public SecurityFilterChain securityFilterChain(){
+
+    }
 }
